a959c36cd122de74e46df45cdabdb4a53f024c6c
minseong-Kang <80028813+minseong00@users.noreply.github.com>
2024-04-09 10:29:01 +0900

    RoomList.jsp 폼 작성 완료
    데이터 출력 처리 코드 삽입 필요

--- src/main/webapp/util/Calendar.js
+++ src/main/webapp/util/Calendar.js
@@ -32,7 +32,7 @@ $(function() {
 	    console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
 	});
 	
-	$('input[name="dateOne"]').daterangepicker({
+	$('.filter').daterangepicker({
 	    "locale": {
 	        "format": "YYYY-MM-DD",
 	        "separator": " ~ ",
