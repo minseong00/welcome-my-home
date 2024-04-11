620844b3d89c986cf4fb1fde7da1203d4caca727
eunjeong <prancispell@naver.com>
2024-04-11 10:42:39 +0900

    roomimgDAO, roomAdd, roomModify, roomadd.jsp, roomModify.jsp
    roomImgDAO : 오타 수정
    roomadd : 리다이랙트 삭제, 사진이 없을 경우 null값이 들어가는 조건문 추가
    roommodify : 리다이렉트 삭제,사진이 없을 경우 null값이 들어가는 조건문 추가
    roomadd.jsp : window location추가,
    roommodify.jsp : window location추가

--- src/main/java/servlets/room/RoomModify.java
+++ src/main/java/servlets/room/RoomModify.java
@@ -114,16 +114,15 @@ public class RoomModify extends HttpServlet {
 		roomDAO.roomUpdate(roomVO);
 		
 		imgVO.setRoomNo(Integer.parseInt(request.getParameter("roomNo")));
-		imgVO.setImg1(list.get(1));
-		imgVO.setImg2(list.get(2));
-		imgVO.setImg3(list.get(3));
-		imgVO.setImg4(list.get(4));
-		imgVO.setImg5(list.get(5));
-		imgVO.setInfoImg(list.get(0));
+		imgVO.setInfoImg(list.size() > 0 ? list.get(0) : null);
+		imgVO.setImg1(list.size() > 1 ? list.get(1) : null);
+		imgVO.setImg2(list.size() > 2 ? list.get(2) : null);
+		imgVO.setImg3(list.size() > 3 ? list.get(3) : null);
+		imgVO.setImg4(list.size() > 4 ? list.get(4) : null);
+		imgVO.setImg5(list.size() > 5 ? list.get(5) : null);
 		
 		imgDAO.updateImg(imgVO);
 		
-		response.sendRedirect("RoomList");
 	}
 
 }
